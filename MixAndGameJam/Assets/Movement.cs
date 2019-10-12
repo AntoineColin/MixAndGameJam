489c8d76e2643170166855f7c984cccef4bf74a4
Lakkar <lucas.gx@hotmail.com>
2019-10-12 14:16:27 +0200

    Movement script

---
+++ MixAndGameJam/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float position;
+    private Rigidbody2D rgbd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position.x;
+        rgbd = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rgbd.velocity=(movement * speed);
+    }
+}
