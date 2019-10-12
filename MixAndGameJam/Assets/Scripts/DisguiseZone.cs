35c74da0e75c5bc252d0445de81ab2c6a631984f
Antoine <antoine35.colin@gmail.com>
2019-10-12 20:05:30 +0200

    Zone de changement de couleur

---
+++ MixAndGameJam/Assets/Scripts/DisguiseZone.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DisguiseZone : MonoBehaviour
+{
+    UnityEvent onDisguiseZone = new UnityEvent(), onNonDisguiseZone = new UnityEvent();
+    bool activable = false;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Action") && activable)
+        {
+            SpriteRenderer characterRenderer = GameObject.Find("Thief").GetComponent<SpriteRenderer>();
+            SpriteRenderer disguiseRenderer = gameObject.GetComponent<SpriteRenderer>();
+            Debug.Log("character : " + characterRenderer.color + " et deguisement : " + disguiseRenderer.color);
+            Color exchange = characterRenderer.color;
+            characterRenderer.color = disguiseRenderer.color;
+            disguiseRenderer.color = exchange;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            activable = true;
+            if (onDisguiseZone != null) onDisguiseZone.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            activable = false;
+            if (onNonDisguiseZone != null) onNonDisguiseZone.Invoke();
+        }
+    }
+}
