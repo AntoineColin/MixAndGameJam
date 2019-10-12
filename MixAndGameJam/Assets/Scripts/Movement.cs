67cf6df24b3609fb3cc9283dc69cf3795e46cfb7
Lakkar <lucas.gx@hotmail.com>
2019-10-12 14:48:47 +0200

    Changement de couleur dans Movement

--- MixAndGameJam/Assets/Scripts/Movement.cs
+++ MixAndGameJam/Assets/Scripts/Movement.cs
@@ -7,12 +7,14 @@ public class Movement : MonoBehaviour
     public float speed;
     float position;
     Rigidbody2D rgbd;
+    SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         position = transform.position.x;
         rgbd = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         
     }
 
@@ -22,5 +24,12 @@ public class Movement : MonoBehaviour
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         Vector2 movement = new Vector2(moveHorizontal, 0);
         rgbd.velocity=(movement * speed);
+        if (rgbd.velocity != new Vector2 (0, 0))
+        {
+            sprite.color = UnityEngine.Color.red;
+        }else
+        {
+            sprite.color = UnityEngine.Color.blue;
+        }
     }
 }
